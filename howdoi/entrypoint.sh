a1ddce0905f96c913cceca106c5584e0e9cfeb5b
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2021-10-22 22:16:29 +0200

    feat: add output

---
+++ howdoi/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# see https://github.com/kvz/bash3boilerplate
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+
+
+echo "::set-output name=answer::$(howdoi $@)"
