34425e8557001a4b1bb246339a4c88f3971733cc
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2021-10-23 00:32:40 +0200

    fix: fix docker image

--- howdoi/entrypoint.sh
+++ howdoi/entrypoint.sh
@@ -3,12 +3,12 @@
 
 # Exit on error. Append "|| true" if you expect an error.
 set -o errexit
-# Exit on error inside any functions or subshells.
-set -o errtrace
-# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
-set -o nounset
-# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
-set -o pipefail
 
+GITHUB_ENV=${GITHUB_ENV:-/tmp/github_env}
 
-echo "::set-output name=answer::$(howdoi $@)"
+ANSWER="$(howdoi $@)"
+echo "ANSWER<<EOF" >> $GITHUB_ENV
+echo "${ANSWER}" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
+
+echo "$ANSWER"
